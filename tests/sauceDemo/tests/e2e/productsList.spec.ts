d5627f723b9c8e95a1c84db87b4c9f23c590fef2
Pablo Vergara <vergara.pablo0616@gmail.com>
2025-09-17 17:40:52 -0400

    added additional tests for product list check

--- tests/sauceDemo/tests/e2e/productsList.spec.ts
+++ tests/sauceDemo/tests/e2e/productsList.spec.ts
@@ -5,13 +5,28 @@ import userData from '../data/userData';
 
 let onHomePage: HomePage;
 let onProductList: ProductListPage;
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  onHomePage = new HomePage(page);
+  onProductList = new ProductListPage(page)
+});
 test.describe('Swag Labs - Product List Page UI Check', () => {
     test('Product List Page is to spec', async ({ page }) => {
-        onHomePage = new HomePage(page);
-        onProductList = new ProductListPage(page);
-
-        await page.goto('https://www.saucedemo.com/');
         await onHomePage.submitLogin(userData.username, userData.password);
         onProductList.checkUI;
   })
+    test('Product Name for "Sauce Labs T-Shirt" is unchanged', async ({ page }) => {
+        await onHomePage.submitLogin(userData.username, userData.password);
+        onProductList.checkProductName;
+  })
+    test('Product Description for "Sauce Labs Backpack" is free of typos', async ({ page }) => {
+        await onHomePage.submitLogin(userData.username, userData.password);
+        onProductList.checkProductCopy;
+  })
+
+    test('Product Price for "Sauce Lab Fleece Jacket" is unchanged', async ({ page }) => {
+        await onHomePage.submitLogin(userData.username, userData.password);
+        onProductList.checkProductPrice;
+  })
+
 });
