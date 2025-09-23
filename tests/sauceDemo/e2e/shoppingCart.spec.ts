93e02ae310b209a891777367f7cc044b3123b36f
Pablo Vergara <vergara.pablo0616@gmail.com>
2025-09-23 19:39:56 -0400

    pages and test for cart page

---
+++ tests/sauceDemo/e2e/shoppingCart.spec.ts
@@ -0,0 +1,31 @@
+import { test} from '@playwright/test';
+import HomePage from '../pages/homePage.ts';
+import ProductListPage from '../pages/productListPage';
+import GlobalHeader from '../pages/globalHeader.ts'
+import CartPage from '../pages/cartPage.ts'
+
+import userData from '../data/userData';
+
+let onHomePage: HomePage;
+let onProductListPage: ProductListPage;
+let onGlobalHeader: GlobalHeader;
+let onCartPage: CartPage;
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  onHomePage = new HomePage(page);
+  onProductListPage = new ProductListPage(page);
+  onGlobalHeader = new GlobalHeader(page);
+  onCartPage = new CartPage(page);
+});
+
+test.describe('Swag Labs - Products Added To Cart UI Check', () => {
+    test('Cart Page is to spec', async ({ page }) => {
+        await onHomePage.submitLogin(userData.username, userData.password);
+        await onProductListPage.clickAddToCart;
+        await onGlobalHeader.clickCartIcon;
+        await onCartPage.checkUI;
+        await onCartPage.checkCopy;
+        await onCartPage.confirmButtonsAreActionable;
+  })
+})
--- tests/sauceDemo/pages/globalHeader.ts
+++ tests/sauceDemo/pages/globalHeader.ts
@@ -1,17 +1,24 @@
 import { type Page, type Locator , expect } from '@playwright/test';
 
-class HeaderPage {
+class GlobalHeader {
     readonly page: Page;
-    readonly #: Locator;
+    readonly cartIcon: Locator;
 
     constructor(page: Page) {
         this.page = page;
+        this.cartIcon = page.locator('//a[@data-test="shopping-cart-link"]');
     }
 
 
-async action(){
-    await expect(this.something).toBeVisible;
+async checkUI(){
+    await expect(this.cartIcon).toBeVisible;
   }
+
+async clickCartIcon(){
+    this.cartIcon.click();
 }
 
-export default HeaderPage;
+
+}
+
+export default GlobalHeader;
