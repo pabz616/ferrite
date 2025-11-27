621bb3b4d7f0d94da966b4003542249cb1e68e2e
Pablo Vergara <vergara.pablo0616@gmail.com>
2025-11-26 20:01:45 -0500

    Fixed failing API test by updating playwright.config file

--- tests/sauceDemo/api/test_bookings.spec.ts
+++ tests/sauceDemo/api/test_bookings.spec.ts
@@ -5,32 +5,32 @@ const { faker } = require('@faker-js/faker')
 test.describe('RESTFUL BOOKER API', () =>{
     test('GET - Get All Bookings', async ({ request}) =>{
     const resp = await request.get(apiData.apiURL+'/booking')
-    const bookings = await resp.json();
+    const booking = await resp.json();
      //CONFIRM API RESPONSE STATUS
      expect(resp.ok()).toBeTruthy();
      expect(resp.status()).toBe(200);
      
      //CONFIRM RESPONSE OUTPUT
-     expect(bookings).toBeDefined();
+     expect(booking).toBeDefined();
    });
 
-test('GET - Get Specific Booking', async ({ request}) =>{
+   test('GET - Get Specific Booking', async ({ request}) =>{
      const bookingId = faker.string.numeric(1)
-     console.log(bookingId)
      const resp = await request.get(apiData.apiURL+'/booking/'+bookingId)
-     const bookings = await resp.json();
+     
+     const booking = await resp.json();
      
      //CONFIRM API RESPONSE STATUS
-     // expect(resp.ok()).toBeTruthy();
+     expect(resp.ok()).toBeTruthy();
      expect(resp.status()).toBe(200);
           
      //CONFIRM RESPONSE OUTPUT
-     // expect(bookings).toBeDefined();
-     // expect(bookings["firstname"]).not.toBeNull()
-     // expect(bookings["lastname"]).not.toBeNull()
-     // expect(bookings["totalprice"]).not.toBeNaN()
-     // expect(bookings["bookingdates"]).not.toBeNull()
-   });
+     expect(booking).toBeDefined();
+     expect(booking["firstname"]).not.toBeNull()
+     expect(booking["lastname"]).not.toBeNull()
+     expect(booking["totalprice"]).not.toBeNaN()
+     expect(booking["bookingdates"]).not.toBeNull()
+  });
 
    test('GET - Filter Bookings By Name', async ({request}) =>{
      const resp = await request.get(apiData.apiURL+'/booking?firstname=Jim&lastname=Jones')
