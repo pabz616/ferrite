fbf9478031edf1bd9a931ccc7b8dba5f1a3b3a31
Pablo Vergara <vergara.pablo0616@gmail.com>
2025-12-03 14:11:38 -0500

    updated YAML file to run typescript tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,9 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -29,7 +29,11 @@ export default defineConfig({
     /* baseURL: '',*/
 
     testIdAttribute: 'data-test',
-    
+    extraHTTPHeaders:{
+      'Accept': '*/*',
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic YWRtaW46cGFzc3dvcmQxMjM='
+    },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
